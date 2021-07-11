5a30c60a9506f244a59511c5ba31749333a6f700
ALEXANDER <msumba83@gmail.com>
2021-07-11 08:26:36 +0200

    done all levels

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const cors = require("cors");
+const { text } = require("express");
 
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.use(cors());
+app.use(express.json);
+app.use(express.urlencoded({ extended: true }));
 
 const welcomeMessage = {
   id: 0,
-  from: "Bart",
+  from: "Alexander",
   text: "Welcome to CYF chat system!",
 };
 
@@ -20,9 +25,72 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
 
-
-app.listen(3000, function () {
-  console.log('Server is listening on port 3000. Ready to accept requests !');
+app.post("/messages", (req, res) => {
+  const newMessage = {
+    id: "",
+    ...req.body,
+  };
+  if (
+    !newMessage.from ||
+    newMessage.from === "" ||
+    !newMessage.text ||
+    newMessage.text === ""
+  ) {
+    return res.status(400).json({ msg: "Please text a message!" });
+  }
+  messages.push(newMessage);
+  messages.forEach((msg, index) => (msg.id = index + 1));
+  res.json(messages);
 });
 
-app.listen(process.env.PORT);
+// Getting all messages
+
+app.get("/messages", (req, res) => res.send(messages));
+
+// searching messages
+
+app.get("/messages/search", (req, res, next) => {
+  const text = req.query.text;
+
+  if (text) {
+    const searchMsg = messages.filter((msg) => {
+      return msg.text.toLowerCase().includes(text.toLowerCase());
+    });
+    res.send(searchMsg);
+  } else {
+    res.send([]);
+  }
+});
+
+// Getting a message by Id
+const messageId = (req) => (message) => message.id === parseInt(req.params.id);
+app.get("/messages:id", (req, res) => {
+  const foundMsg = messages.some(messageId(req));
+
+  if (foundMsg) {
+    res.json(messages.filter(messageId(req)));
+  } else {
+    res.status(400).json({ msg: `No message with the Id: ${req.params.id}` });
+  }
+});
+
+// Delete a message by id
+app.delete("/messages/:id", (req, res) => {
+  const deleteMsg = messages.some(messageId(req));
+
+  if (deleteMsg) {
+    res.json({
+      msg: `Message deleted with id: ${req.params.id}`,
+      messages: messages.filter((message) => !messageId(req)(message)),
+    });
+  } else {
+    res.status(400).json({ msg: `No message with the id: ${req.params.id}` });
+  }
+});
+
+/*app.listen(3000, function () {
+  console.log("Server is listening on port 3000. Ready to accept requests !");
+});*/
+
+app.listen(PORT, () => console.log(`Server started at port: ${PORT}`));
+//app.listen(process.env.PORT);
